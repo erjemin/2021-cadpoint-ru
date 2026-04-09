18e32a2a0f616aa3caaac92850205f5538bd18ce
erjemin <erjemin@gmail.com>
2026-04-09 14:46:49 +0300

    mod: upgrade django to 5.2

--- cadpoint/cadpoint/settings.py
+++ cadpoint/cadpoint/settings.py
@@ -24,7 +24,7 @@ def _normalize_admin_url(value: str) -> str:
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('DJANGO_SECRET_KEY', default='django-insecure-change-me')
@@ -99,7 +99,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'cadpoint.wsgi.application'
 
 # Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', },
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', },
@@ -109,12 +109,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/3.2/topics/i18n/
+# https://docs.djangoproject.com/en/5.2/topics/i18n/
 LANGUAGE_CODE = 'ru-RU'         # <--------- RUSSIAN
 # TIME_ZONE = 'Etc/GMT+3'       #
 TIME_ZONE = 'Europe/Moscow'     #
 USE_I18N = True
-USE_L10N = True
 USE_TZ = True                   # учитывать часовой пояс
 FIRST_DAY_OF_WEEK = 1           # неделя начинается с понедельника
 DEFAULT_CHARSET = 'utf-8'
@@ -176,6 +175,6 @@ EMAIL_USE_TLS = True
 EMAIL_SUBJECT_PREFIX = '[CADPOINT.RU]: '  # префикс для оповещений об ошибках и необработанных исключениях
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
